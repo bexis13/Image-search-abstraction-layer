1f88d2d12c84ad31d15ce0de4ca8a9a170032ebb
Abose U <bexis18@gmail.com>
2017-06-25 19:49:36 +0000

    add comments to layer structure for future work

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 var mlabUrl = process.env.MONGOLAB_URI + "image-search-abstraction-layer" ; /*get mlab url(credentials) from environment variables and url-shortner is dbname*/
 console.log(mlabUrl);
 mongoose.Promise = global.Promise;
-mongoose.connect(mlabUrl || "mongodb://localhost:27017/imageSearchAbstractionLayer"); /*connect to mongodb(on mlab, no locally) with mongoose
-either on mlab cloud remote database or locally installed mongodb*/
+mongoose.connect(mlabUrl || "mongodb://localhost:27017/imageSearchAbstractionLayer"); /*connect to mongodb(on mlab, not locally) with mongoose
+either on mlab cloud remote database or on locally installed mongodb*/
 
 var connection = mongoose.connection;
 connection.on('error', console.error.bind(console, 'connection error:'))
@@ -21,8 +21,18 @@ connection.on('open', function(){
     console.log("connected correctly to the database");
 })
 
-app.get("/", function(request, response){
-    response.send("coming soon...");
+app.get("/new/:search", function(request, response){
+    //get the search parameter from user
+    
+    //save it in the the db
+    
+    //query the image search api and send the usrer the json response
+    
+   
+});
+
+app.get("/recent", function(request, response){
+    //get last ten recent searches from the db
    
 });
 
