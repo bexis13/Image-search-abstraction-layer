c58358e42698f95a5a86ed48b4fbb9772f9dd1d3
Abose U <bexis18@gmail.com>
2017-06-26 10:42:32 +0000

    remove unecessary console logs

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.use(router);
 
 //connect to the database
 const mlabUrl = process.env.MONGOLAB_URI; /*get mlab url(credentials) from environment variables and url-shortner is dbname*/
-console.log(mlabUrl);
 mongoose.Promise = global.Promise;
 mongoose.connect(mlabUrl || "mongodb://localhost:27017/imageSearchAbstractionLayer"); /*connect to mongodb(on mlab, not locally) with mongoose
 either on mlab cloud remote database or on locally installed mongodb*/
