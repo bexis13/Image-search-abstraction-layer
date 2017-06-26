1c2883840f3631ec9ba7db844c90ca965ddea2e0
Abose U <bexis18@gmail.com>
2017-06-26 13:41:18 +0000

    in api id route, create model from user input, store user search term in db and write structure to return correct json response to user

--- routes/routes.js
+++ routes/routes.js
@@ -1,17 +1,39 @@
 const express = require("express"),
       router  = express.Router(),
-      Bing    = require("node-bing-api");
+      Bing    = require("node-bing-api")({ accKey: process.env.API_KEY }),
+      model   = require("../models/models");
 
 router.get("/api/:id", (request, response)=>{
     //get the search parameter from user
     const searchTerm = request.params.id;
     const query = request.query;
-    console.log(searchTerm);
-    console.log(query);
+    const offset = query.offset;
+    //create model data from user input
+    const data = new model({
+        searchTerm : searchTerm,
+        date : new Date()
+    })
     //save it in the the db
-    
-    //query the image search api and send the usrer the json response
-    
+    data.save((err, docs)=>{
+        if(err){
+            console.error(err);
+        }
+        else if(docs){
+            console.log(docs);
+        }
+    })
+    //query the image search api and send the user the json response
+    Bing.images(searchTerm, {
+        top:10,
+        skip:10
+    },(err, res, body)=>{
+        if (err){
+            console.error(err);
+        }
+        console.log(res);
+        console.log(body);
+        
+    })
    
 });
 
